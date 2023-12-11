a0d6d17b8636352141e55a2659054dd987d5b81e
Amit1332 <amitsinghpatel9747@gmail.om>
2023-12-12 00:16:15 +0530

    first-2

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ app.use(cors({
 }))
 
 app.use('/api/v1/',routes)
-app.get('/api/v1/config/paypal',(req,res)=>{
-    res.send(process.env.PAYPAL_CLIENT_ID)
-})
+
+
+
+
 
 app.listen(`${process.env.PORT}`,async()=>{
     try {
